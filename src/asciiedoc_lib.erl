656311a8a1a149252737b454e8a67cc49f68bccb
eryx67 <eryx67@gmail.com>
2016-01-25 20:57:09 +0500

    fix for R18

--- src/asciiedoc_lib.erl
+++ src/asciiedoc_lib.erl
@@ -80,7 +80,7 @@ throw_error(L, D) ->
 %% expand_text/2
 expand_text(Cs, L) ->
     DirName = filename:join(["/", "tmp", ?MODULE_STRING]),
-    {A,B,C}=now(),
+    {A,B,C}= my_now(),
     Unique = lists:flatten(io_lib:format("~p-~p.~p.~p",[node(),A,B,C])),
     FileName = filename:join([DirName, Unique]),
     In = FileName ++ ".in",
@@ -169,3 +169,11 @@ get_attr(Name, [_ | As]) ->
     get_attr(Name, As);
 get_attr(_, []) ->
     [].
+
+my_now() ->
+    case erlang:function_exported(erlang, timestamp, 0) of
+        true ->
+            erlang:timestamp();
+        false ->
+            apply(erlang, now, [])
+    end.
